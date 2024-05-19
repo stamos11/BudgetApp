75396b76e7da535c126062436a227d1c11c2a35a
Nicholas Stamoulis <nikolaos11@hotmail.com>
2024-05-20 01:23:50 +0300

    Initial Commit

---
+++ BudgetApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BudgetApp
+//
+//  Created by stamoulis nikolaos on 20/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
